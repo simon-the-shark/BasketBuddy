6fa29f6241d97d87e1522f31080640cde1e27998
Szymon Kowaliński <szymon@kowalinski.dev>
2024-12-16 16:11:37 +0100

    feat: cache products

--- BasketBuddy/Products/Model/ProductsRepository.swift
+++ BasketBuddy/Products/Model/ProductsRepository.swift
@@ -1,25 +1,30 @@
-//
-//  ProductsRepository.swift
-//  BasketBuddy
-//
-//  Created by Szymon Kowaliński on 08/12/2024.
-//
-
 import Foundation
 
 class ProductsRepository {
     static let shared = ProductsRepository()
     private init() {}
 
+    private var cachedProducts: [Product] = []
+
     func getAll(with: AuthService) async -> [Product] {
+        if !cachedProducts.isEmpty {
+            return cachedProducts
+        }
+
         do {
             let (data, _) = try await NetworkingClient.shared.makeRequest(endpoint: "/api/v1/products/", headers: [
                 "Authorization": "Token \(with.authState.tokenRequired)",
             ])
-            return try JSONDecoder().decode([Product].self, from: data)
+            let products = try JSONDecoder().decode([Product].self, from: data)
+            cachedProducts = products
+            return products
 
         } catch {
             return []
         }
+    }
+
+    func clearCache() {
+        cachedProducts.removeAll()
     }
 }
